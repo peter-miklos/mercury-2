be6aa044a15df0aecbacb02a46970b64c4129571
Peter Miklos <pt.miklos@gmail.com>
2017-01-16 19:31:27 +0000

    app component updated, app.router added

---
+++ client/app/app.router.ts
@@ -0,0 +1,7 @@
+import { LoginComponent }       from './login.component';
+import { AllProductsComponent } from './allProducts.component';
+
+export const routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'allproducts', component: AllProductsComponent }
+];
