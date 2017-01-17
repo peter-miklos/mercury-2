f1cd14c9a30b08960211e047c9fc7d0bd36106df
Peter Miklos <pt.miklos@gmail.com>
2017-01-17 14:52:02 +0000

    app.routes renamed to app.routing, content updated

---
+++ client/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent }       from './login/login.component';
+import { HomeComponent }        from './home/home.component';
+import { AuthGuard }            from './_guards/auth.guard';
+import { AllProductsComponent } from './products/allProducts.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'allproducts', component: AllProductsComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '*', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
