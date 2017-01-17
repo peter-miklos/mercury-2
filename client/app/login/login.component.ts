8da3ff8f66b460419464171725a503539f3a2771
Peter Miklos <pt.miklos@gmail.com>
2017-01-17 18:00:02 +0000

    login component files finalized, app.component and index.html updated

--- client/app/login/login.component.ts
+++ client/app/login/login.component.ts
@@ -1,7 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit }      from '@angular/core';
+import { Router }                 from '@angular/router';
+
+import { AuthenticationService }  from '../_services/authentication.service';
 
 @Component({
-  selector: 'login',
-  template: `<h2>Login</h2>`,
+  moduleId: module.id,
+  templateUrl: 'login.component.html'
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  model: any = {};
+  loading = false;
+  error = '';
+
+  constructor (
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
+
+  ngOnInit() {
+    this.authenticationService.logout();
+  }
+
+  login() {
+    this.loading = true;
+    this.authenticationService.login(this.model.username, this.model.password)
+        .subscribe(result => {
+          if (result === true) {
+            this.router.navigate(['/']);
+          } else {
+            this.error = 'Username or password is incorrect';
+            this.loading = false;
+          }
+        });
+  }
+}
