d6306c2ff08e3ab3ec5fc4e05a7881d76ba4c3aa
Peter Miklos <pt.miklos@gmail.com>
2017-01-18 19:17:21 +0000

    new product can be registered

--- client/app/products/product-form.component.ts
+++ client/app/products/product-form.component.ts
@@ -1,4 +1,7 @@
-import { Component }              from '@angular/core';
+import { Component }        from '@angular/core';
+import { Location }         from '@angular/common';
+
+import { ProductService }   from '../_services/product.service';
 
 @Component({
   moduleId: module.id,
@@ -8,5 +11,19 @@ import { Component }              from '@angular/core';
 })
 
 export class ProductFormComponent {
+  model: any = {};
 
+  constructor(
+    private productService: ProductService,
+    private location: Location
+  ){}
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  submit(): void {
+    this.productService.create(this.model)
+        .then(() => this.goBack())
+  }
 }
