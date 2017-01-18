fb283a718dbc9e4e865a39fd6803693eea453694
Peter Miklos <pt.miklos@gmail.com>
2017-01-18 15:07:41 +0000

    basic nav menu added to app.component

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <div>
-    <router-outlet></router-outlet>
-  </div>
+  <nav>
+    <a routerLink="/" routerLinkActive="active">Home</a> |
+    <a routerLink="/products" routerLinkActive="active">All products</a> |
+    <a routerLink="/products/new" routerLinkActive="active">Add new product</a>
+  </nav>
+  <router-outlet></router-outlet>
   `,
 })
 export class AppComponent {}
