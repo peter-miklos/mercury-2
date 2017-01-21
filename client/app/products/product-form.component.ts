746763e946ce65a2e0b0811220964c5811c518e1
Peter Miklos <pt.miklos@gmail.com>
2017-01-21 19:26:03 +0000

    product-form.component updated with some validations

--- client/app/products/product-form.component.ts
+++ client/app/products/product-form.component.ts
@@ -1,5 +1,6 @@
-import { Component }        from '@angular/core';
-import { Location }         from '@angular/common';
+import { Component }                          from '@angular/core';
+import { Location }                           from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { ProductService }   from '../_services/product.service';
 
@@ -11,21 +12,31 @@ import { ProductService }   from '../_services/product.service';
 })
 
 export class ProductFormComponent {
-  private model: any = {};
+  // private model: any = {};
+  private productForm: FormGroup;
   private loading = false;
 
   constructor(
     private productService: ProductService,
-    private location: Location
-  ){}
+    private location: Location,
+    private fb: FormBuilder
+  ){
+    this.productForm = fb.group({
+      'name': [null, Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(20)])],
+      'category': [null, Validators.required],
+      'group': [null, Validators.required],
+      'price': [null, Validators.required],
+      'origin': [null, Validators.required]
+    })
+  }
 
   goBack(): void {
     this.location.back();
   }
 
-  submit(): void {
+  submit(value: any): void {
     this.loading = true;
-    this.productService.create(this.model)
+    this.productService.create(value)
         .then(() => {
           this.loading = false;
           this.goBack();
